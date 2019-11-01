8ff9bc0026f069bf6468f3af685fa6aedc3d5b56
maxter <max.ternovyy@gmail.com>
2019-11-01 11:46:27 +0100

    courceitem-component-testing

--- task1/src/app/courceitem/courceitem.component.spec.ts
+++ task1/src/app/courceitem/courceitem.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CourceItem } from '../cource-item';
 import { CourceitemComponent } from './courceitem.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('CourceitemComponent', () => {
-  let component: CourceitemComponent;
+  let sut: CourceitemComponent;
   let fixture: ComponentFixture<CourceitemComponent>;
+
+  let courceItem: CourceItem;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,12 +18,34 @@ describe('CourceitemComponent', () => {
   }));
 
   beforeEach(() => {
+    courceItem = new CourceItem();
+    courceItem.Id = 1;
+    courceItem.Title= "lesson 1";
+    courceItem.ConductAt = new Date('04.01.2020');
+    courceItem.DurationMin = 50
+    courceItem.Description = "Some description 1";
+
+
     fixture = TestBed.createComponent(CourceitemComponent);
-    component = fixture.componentInstance;
+    sut = fixture.componentInstance;
+    sut.courceItem = courceItem;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create component item', () => {
+    expect(sut).toBeTruthy();
   });
+
+
+  it('should have a cource title', () => {
+    fixture.detectChanges();
+
+    const debugElement: DebugElement = fixture.debugElement;
+    const captionDebugElement = debugElement.query(By.css('.caption'));
+    const cap = captionDebugElement.nativeElement;
+
+    expect(cap.textContent).toBe(courceItem.Title);
+  });
+
+
 });
