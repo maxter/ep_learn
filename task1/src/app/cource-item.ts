07552d97ca09183c8a761c6e06fe48910a3cf1ac
MaxTernovy <max.ternovyy@gmail.com>
2019-11-05 12:28:32 +0100

    constructor

--- task1/src/app/cource-item.ts
+++ task1/src/app/cource-item.ts
@@ -8,14 +8,14 @@ export class CourceItem implements ICourceItem {
     Description:string;
 
 
-    constructor(id:number = null, title: string  = null, conductAt:Date  = null, durationMin:number  = null, description:string  = null) {
+    constructor(id:number = null, title: string  = null, conductAt:string  = null, durationMin:number  = null, description:string  = null) {
         
         if(id == null || title == null || conductAt == null || durationMin == null || description==null)
           return;
-          
+
         this.Id = id;
         this.Title = title;
-        this.ConductAt = conductAt;
+        this.ConductAt = new Date(conductAt);
         this.DurationMin = durationMin;
         this.Description = description;
     }
