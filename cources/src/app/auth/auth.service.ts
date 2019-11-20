a9ad5f6d5517febf017affd344ed69820d87a2c7
MaxTernovy <max.ternovyy@gmail.com>
2019-11-20 16:59:00 +0100

    auth-guard

--- cources/src/app/auth/auth.service.ts
+++ cources/src/app/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class AuthService {
-
-  constructor() { }
+  constructor(public jwtHelper: JwtHelperService) {}
+  // ...
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('token');
+    // Check whether the token is expired and return
+    // true or false
+    return !this.jwtHelper.isTokenExpired(token);
+  }
 }
