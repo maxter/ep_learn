816ec418b5d23304e55c945c86e2c4c8ba2e3a4a
MaxTernovy <max.ternovyy@gmail.com>
2019-11-22 13:47:18 +0100

    auth-localstorage

--- cources/src/app/auth/auth.module.ts
+++ cources/src/app/auth/auth.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AuthGuardService } from './auth-guard.service';
 import { AuthService } from './auth.service';
-
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,FormsModule
   ],
   providers:[AuthService, AuthGuardService],
   exports: [LoginComponent]
--- cources/src/app/auth/login/login.component.ts
+++ cources/src/app/auth/login/login.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public userName : string = "";
+  public password : string = "";
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  
+  login() : void {
+    //save to localstorage
+    if(this.userName=="user" && this.password=="12345")
+       {
+        localStorage.setItem('isAuth',"true");
+
+       }
+   }
+
 }
+
+
+
