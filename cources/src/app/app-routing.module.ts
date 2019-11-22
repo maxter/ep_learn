33f172597e616f6eaadf12af99259b1e3dbc68fd
MaxTernovy <max.ternovyy@gmail.com>
2019-11-22 12:40:21 +0100

    redirect-to-login

--- cources/src/app/app-routing.module.ts
+++ cources/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CourcesComponent } from './cources/cources.component';
+import { LoginComponent } from './auth/login/login.component';
 import { AuthGuardService } from './auth/auth-guard.service';
 
 const routes: Routes = [
-  {path : 'auth', loadChildren : './auth/auth.module#AuthModule'},
+  { path : 'login', component: LoginComponent},
   { path: 'cources', component: CourcesComponent, canActivate: [AuthGuardService]  },
   { path: '', redirectTo: '/cources', pathMatch: 'full' }
 ];
--- cources/src/app/auth/auth-guard.service.ts
+++ cources/src/app/auth/auth-guard.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
-//import { AuthService } from './auth.service';
+//import { AuthService } from '.auth//auth.service';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
-  //constructor(public auth: AuthService, public router: Router) {}
+ // constructor(public auth: AuthService, public router: Router) {}
   constructor(public router: Router) {}
   canActivate(): boolean {
 
 
     return true;
+  //  this.router.navigate(['login']);
+    return false;
 
  /*   if (!this.auth.isAuthenticated()) {
       this.router.navigate(['login']);
