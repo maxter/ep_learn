6367daca79d914f660ed7cf5156c1a9c7dd248cc
MaxTernovy <max.ternovyy@gmail.com>
2020-01-23 12:56:31 +0100

    update_effect

--- cources/src/app/app.component.ts
+++ cources/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {select, Store} from '@ngrx/store';
-import {Observable} from 'rxjs';
-import {LOAD_COURCES} from './redux/cources.actions';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { LOAD_COURCES, CourceSave } from './redux/cources.actions';
 import { CourceItem } from './cource-item';
 
 @Component({
@@ -13,9 +13,40 @@ export class AppComponent {
   title = 'task1';
 
   constructor(private store: Store<{ cources: CourceItem[] }>) {
-    this.store.dispatch(new LOAD_COURCES(null));
-  }
-  
-  
+    console.log(' APP INIT ');
+    // this.store.dispatch(new LOAD_COURCES({ id: 123 }));
+    const newCourse = new CourceItem(
+      22,
+      'test Title',
+      '12.12.2012',
+      33,
+      'asdasdasd description',
+      false)
 
+    this.store.dispatch(new CourceSave(newCourse));
+  }
 }
+
+
+// import { Component } from '@angular/core';
+// import {select, Store} from '@ngrx/store';
+// import {Observable} from 'rxjs';
+// import {LOAD_COURCES} from './redux/cources.actions';
+// import { CourceItem } from './cource-item';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'task1';
+
+//   constructor(private store: Store<{ cources: CourceItem[] }>) {
+//     this.store.dispatch(new LOAD_COURCES(null));
+//   }
+  
+// }
+
+
+
--- cources/src/app/redux/cources.actions.ts
+++ cources/src/app/redux/cources.actions.ts
@@ -4,7 +4,8 @@ export enum CourcesActionTypes {
   Add = '[Cource Component] Add',
   Remove = '[Cource Component] Remove',
   LoadCourcesSuccess = '[Cource Component] LoadCourcesSuccess',
-  LOAD_COURCES ='[Cource Component] LoadCources'
+  LOAD_COURCES ='[Cource Component] LoadCources',
+  Save = '[Cource Component] Save',
 }
 export class ActionEx implements Action {
   readonly type;
@@ -32,3 +33,51 @@ export class LOAD_COURCES implements ActionEx {
   constructor(public payload: any) {
   }
 }
+
+export class CourceSave implements ActionEx {
+  readonly type = CourcesActionTypes.Save;
+  constructor(public payload: any) {
+  }
+}
+
+
+
+
+
+
+
+
+// import {Action} from '@ngrx/store';
+
+// export enum CourcesActionTypes {
+//   Add = '[Cource Component] Add',
+//   Remove = '[Cource Component] Remove',
+//   LoadCourcesSuccess = '[Cource Component] LoadCourcesSuccess',
+//   LOAD_COURCES ='[Cource Component] LoadCources'
+// }
+// export class ActionEx implements Action {
+//   readonly type;
+//   payload: any;
+// }
+// export class CourceAdd implements ActionEx {
+//   readonly type = CourcesActionTypes.Add;
+//   constructor(public payload: any) {
+//   }
+// }
+// export class CourceRemove implements ActionEx {
+//   readonly type = CourcesActionTypes.Remove;
+//   constructor(public payload: any) {
+//   }
+// }
+ 
+// export class LoadCourcesSuccess implements ActionEx {
+//   readonly type = CourcesActionTypes.LoadCourcesSuccess;
+//   constructor(public payload: any) {
+//   }
+// }
+
+// export class LOAD_COURCES implements ActionEx {
+//   readonly type = CourcesActionTypes.LOAD_COURCES;
+//   constructor(public payload: any) {
+//   }
+// }
