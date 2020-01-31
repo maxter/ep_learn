5b64b0f12cedee222ae04068a8dc481182a71f43
maxter <max.ternovyy@gmail.com>
2020-01-31 14:58:07 +0100

    forms

--- cources/src/app/update-item/update-item.component.ts
+++ cources/src/app/update-item/update-item.component.ts
@@ -12,6 +12,9 @@ import {select, Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
 import {CourceSave} from '../redux/cources.actions';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-update-item',
   templateUrl: './update-item.component.html',
@@ -19,6 +22,8 @@ import {CourceSave} from '../redux/cources.actions';
   providers: [CourcesService]
 })
 export class UpdateItemComponent implements OnInit {
+
+  courceForm: FormGroup;
 
   cources$: Observable<CourceItem[]>;
 
@@ -28,11 +33,14 @@ export class UpdateItemComponent implements OnInit {
   public courceItem : ICourceItem;
   public pageTitle : string;
   
-  constructor(private store: Store<{ cources: CourceItem[] }>, private route: ActivatedRoute, private courcesService: CourcesService, private router: Router) { 
+  constructor(private fb: FormBuilder, private store: Store<{ cources: CourceItem[] }>, private route: ActivatedRoute, private courcesService: CourcesService, private router: Router) { 
     this.cources$ = store.pipe(select('cources'));
   }
 
   ngOnInit() {
+
+    this.initForm();
+
     this.sub = this.route.params.subscribe(params => {
       this.id = +params['id']; 
       this.isNew = false;
@@ -48,6 +56,57 @@ export class UpdateItemComponent implements OnInit {
        this.pageTitle = "Add cource item";
       }
    });
+  }
+
+  initForm(){
+    this.courceForm = this.fb.group({
+     Title: ['cource title', [
+      Validators.required,
+      Validators.maxLength(50)
+     ]
+    ],
+     Description: [null, [
+      Validators.required,
+      Validators.maxLength(500)
+     ]
+    ],
+     Length: [null,
+      [
+        Validators.required,
+        Validators.pattern('^[0-9]+$')
+       ]]
+    });
+  }
+
+  isControlInvalid(controlName: string): boolean {
+     const control = this.courceForm.controls[controlName];
+     const result = control.invalid && control.touched;
+     return result;
+  }
+
+  onSubmit() {
+    const controls = this.courceForm.controls;
+    
+     if (this.courceForm.invalid) {
+
+      Object.keys(controls)
+       .forEach(controlName => controls[controlName].markAsTouched());
+       
+
+       return;
+      }
+
+    const item = new CourceItem();
+    
+    item.Title = this.courceForm.controls["Title"].value;
+    item.DurationMin = this.courceForm.controls["Length"].value;
+    item.Description = this.courceForm.controls["Description"].value;
+
+    this.store.dispatch(new CourceSave(item));
+    this.router.navigate(['/cources']);
+    
+
+     console.log(this.courceForm.value);
   }
 
   ngOnDestroy() {
